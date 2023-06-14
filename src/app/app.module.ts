8cac94ef83e7988bd4fb2363cb57dd2a909230cc
ShineWxrk <ShineWxrk@gmail.com>
2023-06-14 23:19:05 +0300

    add update task category

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -35,6 +37,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    MatOptionModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dialog/edit-task-dailog/edit-task-dailog.component.ts
+++ src/app/dialog/edit-task-dailog/edit-task-dailog.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewContainerRef, ComponentFactoryResolver, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Category } from 'src/app/model/Category';
 import { Task } from 'src/app/model/Task';
+import { DataHandlerService } from 'src/app/service/data-handler.service';
 
 @Component({
   selector: 'app-edit-task-dailog',
@@ -8,10 +10,14 @@ import { Task } from 'src/app/model/Task';
   styleUrls: ['./edit-task-dailog.component.css']
 })
 export class EditTaskDailogComponent implements OnInit {
-  constructor(public dialogRef: MatDialogRef<EditTaskDailogComponent>, @Inject(MAT_DIALOG_DATA)private data: [Task, string]) {}
+  constructor(public dialogRef: MatDialogRef<EditTaskDailogComponent>, @Inject(MAT_DIALOG_DATA)private data: [Task, string], private dataHandler: DataHandlerService,) {}
+
+  task: Task
+
+  categories: Category[]
+  tmpCategory: Category
 
   dialogTitle: string
-  task: Task
   tmpTitle: string
 
   ngOnInit() {
@@ -19,10 +25,15 @@ export class EditTaskDailogComponent implements OnInit {
     this.dialogTitle = this.data[1]
 
     this.tmpTitle = this.task.title
+    if (this.task.category != undefined) {
+      this.tmpCategory = this.task.category
+    }
+    this.dataHandler.getAllCategories().subscribe(items => this.categories = items)
   }
 
   onConfirm(): void {
     this.task.title = this.tmpTitle
+    this.task.category = this.tmpCategory
     this.dialogRef.close(this.task)
   }
 
